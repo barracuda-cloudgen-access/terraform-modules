68a2b8b4ee857685a54537dc088202c5f79958ab
Gabriel Martinez <gabrielmartinez@sisti.pt>
2021-06-02 17:53:45 +0100

    Update naming and allow multiple deploys on the same region

--- modules/aws-asg/outputs.tf
+++ modules/aws-asg/outputs.tf
@@ -1,9 +1,9 @@
 output "Network_Load_Balancer_DNS_Name" {
-  description = "Update the Fyde Access Proxy in the Console with this DNS name"
+  description = "Update the CloudGen Access Proxy in the Console with this DNS name"
   value       = aws_lb.nlb.dns_name
 }
 
 output "Security_Group_for_Resources" {
-  description = "Use this group to allow Fyde Access Proxy access to internal resources"
+  description = "Use this group to allow CloudGen Access Proxy access to internal resources"
   value       = aws_security_group.resources.id
 }
